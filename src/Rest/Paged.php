1e90537225c4f2c1ffb6bc3ef9666469db1c7f8b
Rob Woodward <rob@emailplus.org>
2024-01-22 15:45:24 +0000

    Document Rest Paged function

--- src/Rest/Paged.php
+++ src/Rest/Paged.php
@@ -10,6 +10,8 @@
 
 namespace Robwdwd\ArborApiBundle\Rest;
 
+use Robwdwd\ArborApiBundle\Exception\ArborApiException;
+
 /**
  * Access the Arbor Sightline REST API.
  *
@@ -23,42 +25,94 @@ class Paged extends REST
 
     private int $totalPages = 0;
 
-    private $currentData;
+    private array $currentData = [];
+
+    private string $oururl = '';
+
+    private array $ourArgs = [];
 
     /**
      * Find or search Arbor SP REST API for a particular record or set of
-     * records.
+     * records, uses paging instead of returning full result set.
      *
      * @param string $endpoint   Endpoint type, Managed Object, Mitigations etc.
      *                           See Arbor API documenation for endpoint list.
      * @param array  $filters    Search filters
      * @param int    $perPage    Limit the number of returned objects per page, default 50
      * @param bool   $commitFlag Add config=commited to endpoints which require it, default false
-     *
-     * @return array returns an array with the records from the API
      */
-    public function findRestPaged(string $endpoint, array $filters = null, int $perPage = 50, $commitFlag = false)
+    public function findRestPaged(string $endpoint, array $filters = null, int $perPage = 50, bool $commitFlag = false): void
     {
-        $url = $this->url.$endpoint.'/';
+        // Clear any current arguments for request and any current data.
+        $this->currentData = [];
+        $this->ourArgs = [];
+
+        // Generate new URL.
+        $this->oururl = $this->url.$endpoint.'/';
 
         if (null !== $filters) {
-            $url .= '?'.$this->filterToUrl($filters);
+            $this->oururl .= '?'.$this->filterToUrl($filters);
         }
 
-        $args = ['perPage' => $perPage, 'page' => $this->currentPage];
+        $this->ourArgs['perPage'] = $perPage;
+        $this->ourArgs['page'] = 1;
 
         if (true === $commitFlag) {
-            $args['config'] = 'committed';
+            $this->ourArgs['config'] = 'committed';
         }
 
-        $this->currentData = $this->doGetRequest($url, $args);
+        $result = $this->doGetRequest($this->oururl, $this->ourArgs);
+
+        $this->currentData = $result['data'];
 
         //
         // Work out the number of pages.
         //
-        if (isset($apiResult[0]['links']['last'])) {
-            parse_str(parse_url((string) $apiResult[0]['links']['last'])['query'], $parsed);
+        if (isset($result['links']['last'])) {
+            parse_str(parse_url((string) $result['links']['last'])['query'], $parsed);
             $this->totalPages = $parsed['page'];
+        } else {
+            throw new ArborApiException('Unable to determine the number of pages.');
         }
+    }
+
+    /**
+     * Get next page of date from API. Returns true if there is more data or
+     * false if there are no more pages of data.
+     */
+    public function getNextPage(): bool
+    {
+        // Increment page by one
+        ++$this->ourArgs['page'];
+
+        // Have we gone over the total pages?
+        if ($this->ourArgs['page'] > $this->totalPages) {
+            return false;
+        }
+
+        // Get result data.
+        $result = $this->doGetRequest($this->oururl, $this->ourArgs);
+
+        $this->currentData = $result['data'];
+
+        ++$this->currentPage;
+
+        return true;
+    }
+
+    /**
+     * Get current data from the API request.
+     */
+    public function getCurrentData()
+    {
+        return $this->currentData;
+    }
+
+    /**
+     *  Get the current page number.
+     */
+    public function getCurrentPageNumber()
+    {
+        return $this->currentPage;
     }
 }
